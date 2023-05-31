4e9c8e26754ef4e970214a9e7185274219f6377e
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-05-31 18:44:01 +0200

    cleanup

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { CreateProductDto } from './dto/create-product.dto';
 
 describe('ProductsService', () => {
   let service: ProductsService;
@@ -31,18 +30,5 @@ describe('ProductsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-
-  it('should create product', () => {
-    const productDto: CreateProductDto = {
-      author: 'John Doe',
-      title: 'Test title',
-      price: 15,
-    };
-    const product = service.create(productDto);
-    expect(product).toEqual({
-      id: expect.any(Number),
-      ...productDto,
-    });
   });
 });
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,35 +1,4 @@
 import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { CreateProductDto } from './dto/create-product.dto';
-// import { Product } from './entities/product.entity';
-// import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProductsService {
-  // constructor(
-  //   @InjectRepository(Product)
-  //   private readonly productsRepository: Repository<Product>,
-  // ) {}
-
-  // create(createProductDto: CreateProductDto) {
-  //   const product = this.productsRepository.create(createProductDto);
-  //   return this.productsRepository.save(product);
-  // }
-
-  // findAll() {
-  //   return `This action returns all products`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} product`;
-  // }
-  //
-  // update(id: number, updateProductDto: UpdateProductDto) {
-  //   console.log(updateProductDto);
-  //   return `This action updates a #${id} product`;
-  // }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
-}
+export class ProductsService {}
